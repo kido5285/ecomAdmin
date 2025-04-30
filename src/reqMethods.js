931c1c926c5c5579415c49839f845bad8277f812
kido5285 <75468540+kido5285@users.noreply.github.com>
2025-04-30 17:06:23 +0800

    Update reqMethods.js

--- src/reqMethods.js
+++ src/reqMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'https://ecomapi109.herokuapp.com/api/';
+const BASE_URL = 'https://web-production-1424a.up.railway.app/';
 let Token = process.env.REACT_APP_Token;
 
 export const publicReq = axios.create({
